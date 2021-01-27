ee90bb3a8ca05e28b3d6feaa42c7b7a2cbc08aac
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-01-26 21:13:21 -0800

    added hour values to hour array

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
 
 // get each store element by id.
 
-let hours = ['6am', '7am', '8am'];
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 let seattle = {
   name: 'Seattle',
-  min: 23,
-  max: 65,
-  avg: 6.3,
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
   // a method to calculate random number of customers per hour
   // a method to calculate and populate our number of cookies sold per hour array
   // a methond to render list items
