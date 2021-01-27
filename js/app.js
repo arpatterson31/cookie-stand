b2d9181a48db571be70c8f112c46e7c4cb7c6f2d
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-01-27 08:48:52 -0800

    built first attempt at my methods... i can get th customer per hour to console log, but my 2nd method is undefined..and 3rd doesn't render to the html doc

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 // get each store element by id.
 
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let seattleList = document.getElementById('seattle');
 
 let seattle = {
   name: 'Seattle',
@@ -11,18 +12,25 @@ let seattle = {
   avgCookie: 6.3,
   // a method to calculate random number of customers per hour
   customerPerHour: function () {
-    for (let i = 0; i < hours.length; i++){
-      return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    }
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
   },
   // a method to calculate and populate our number of cookies sold per hour array
   cookieArray: [],
   cookiesPerHour: function (){
-    for (let j = 0; j <)
-    this.customerPerHour * this.avgCookie;
+    for (let i = 0; i <=hours.length; i++){
+      this.cookieArray.push(this.customerPerHour * this.avgCookie);
+    }
   },
   // a method to render list items
   render: function() {
-    for (let i = 0; i < seattle.)
+    for (let j = 0; j < this.cookieArray.length; j++){
+      let li = document.createElement(li);
+      li.textContent = this.cookieArray[j];
+      seattleList.appendChild(li);
+    }
   }
 };
+
+seattle.render();
+console.log(seattle.customerPerHour());
+console.log(seattle.cookiesPerHour());
