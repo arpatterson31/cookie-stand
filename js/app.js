43948db6b2579e9d7f9f2cf97b7d59bea2a73db2
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-01-27 09:00:27 -0800

    updated consolelogs

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 let seattleList = document.getElementById('seattle');
 
-let seattle = {
+let seattleStore = {
   name: 'Seattle',
   minCust: 23,
   maxCust: 65,
@@ -17,7 +17,7 @@ let seattle = {
   // a method to calculate and populate our number of cookies sold per hour array
   cookieArray: [],
   cookiesPerHour: function (){
-    for (let i = 0; i <=hours.length; i++){
+    for (let i = 0; i < hours.length; i++){
       this.cookieArray.push(this.customerPerHour * this.avgCookie);
     }
   },
@@ -31,6 +31,7 @@ let seattle = {
   }
 };
 
-seattle.render();
-console.log(seattle.customerPerHour());
-console.log(seattle.cookiesPerHour());
+seattleStore.render();
+console.log(seattleStore.customerPerHour());
+console.log(seattleStore.cookiesPerHour());
+console.log(seattleStore.cookieArray);
