ce0455dc189841cc1792966e5fb38c8ef23f5400
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-01-26 20:54:38 -0800

    added a few comments and guide lines to js file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// get each store element by id.
+
+let hours = ['6am', '7am', '8am'];
+
+let seattle = {
+  name: 'Seattle',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  // a method to calculate random number of customers per hour
+  // a method to calculate and populate our number of cookies sold per hour array
+  // a methond to render list items
+};
