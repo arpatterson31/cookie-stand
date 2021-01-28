e1eadf1f87103f1562a0096c38a0c041da0b7ba4
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-01-27 18:46:24 -0800

    updated names of my variables for clarity

--- js/app.js
+++ js/app.js
@@ -8,28 +8,29 @@ let seattleList = document.getElementById('seattle');
 
 let seattleStore = {
   name: 'Seattle',
-  minCust: 23,
-  maxCust: 65,
-  avgCookie: 6.3,
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgCookieBought: 6.3,
   // a method to calculate random number of customers per hour
   customerPerHour: function () {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
   },
   // a method to calculate and populate our number of cookies sold per hour array
-  cookieArray: [], // added array to use in render
+  cookieSoldArray: [], // added array to use in render
+  totalCookies: 0, // do math to add the cookie array total
   cookiesPerHour: function (){
     for (let i = 0; i < hours.length; i++){
-      this.cookieArray.push(Math.ceil(this.customerPerHour() * this.avgCookie)); // math.ceil rounds up so it corrected the NaN error I was seeing
+      this.cookieSoldArray.push(Math.ceil(this.customerPerHour() * this.avgCookieBought)); // math.ceil rounds up so it corrected the NaN error I was seeing
     }
   },
   // a method to render list items
   render: function() {
     this.cookiesPerHour(); // had to call method within this method to tell li.textcontent what to enter
-    for (let i = 0; i < this.cookieArray.length; i++){
+    for (let i = 0; i < this.cookieSoldArray.length; i++){
       // create element
       let li = document.createElement('li');
       // give it content
-      li.textContent = `${hours[i]}: ${(this.cookieArray[i])} cookies`;
+      li.textContent = `${hours[i]}: ${(this.cookieSoldArray[i])} cookies`;
       // add it
       seattleList.appendChild(li);
     }
